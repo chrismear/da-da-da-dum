e5397019a12d7d955d9dbc9b0ac0e2ca1b4d6348
Chris Mear <chris@feedmechocolate.com>
2008-10-29 23:51:55 +0000

    Use a randomly-generated code to identify dums.

--- app/models/dum.rb
+++ app/models/dum.rb
@@ -1,2 +1,17 @@
+require 'digest/sha1'
+
 class Dum < ActiveRecord::Base
+  before_create :assign_code
+  
+  def assign_code
+    self.code = Dum.generate_code
+  end
+  
+  def self.generate_code
+    Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )[0..7]
+  end
+  
+  def to_param
+    self.code
+  end
 end
--- spec/models/dum_spec.rb
+++ spec/models/dum_spec.rb
@@ -1,12 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Dum do
+  fixtures :dums
+  
   before(:each) do
     @valid_attributes = {
     }
   end
-
+  
   it "should create a new instance given valid attributes" do
     Dum.create!(@valid_attributes)
+  end
+  
+  it "should generate a random code, eight characters long" do
+    code = Dum.generate_code
+    code.length.should == 8
+  end
+  
+  it "should give itself a new code when created" do
+    dum = Dum.create
+    dum.reload
+    dum.code.should_not be_nil
+  end
+  
+  it "should have a custom param of its code" do
+    dums(:one).to_param.should == "6a2a385d"
   end
 end
